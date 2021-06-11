240c16de0ac2c3b7f1cba44610fae450f6c44840
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 13:04:35 +0200

    Initial commit

---
+++ CS029StopkyTimer/StopkyTimerForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CS029StopkyTimer
+{
+    partial class CS029Stopky
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.casLabel = new System.Windows.Forms.Label();
+            this.startStopButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.stopkyTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // casLabel
+            // 
+            this.casLabel.AutoSize = true;
+            this.casLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.casLabel.Location = new System.Drawing.Point(216, 117);
+            this.casLabel.Name = "casLabel";
+            this.casLabel.Size = new System.Drawing.Size(93, 31);
+            this.casLabel.TabIndex = 0;
+            this.casLabel.Text = "Čas: ?";
+            // 
+            // startStopButton
+            // 
+            this.startStopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.startStopButton.Location = new System.Drawing.Point(441, 180);
+            this.startStopButton.Name = "startStopButton";
+            this.startStopButton.Size = new System.Drawing.Size(155, 55);
+            this.startStopButton.TabIndex = 1;
+            this.startStopButton.Text = "Start";
+            this.startStopButton.UseVisualStyleBackColor = true;
+            this.startStopButton.Click += new System.EventHandler(this.startStopButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.resetButton.Location = new System.Drawing.Point(259, 282);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(168, 61);
+            this.resetButton.TabIndex = 2;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            // 
+            // stopkyTimer
+            // 
+            this.stopkyTimer.Tick += new System.EventHandler(this.stopkyTimer_Tick);
+            // 
+            // CS029Stopky
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.startStopButton);
+            this.Controls.Add(this.casLabel);
+            this.Name = "CS029Stopky";
+            this.Text = "CS029StopkyTimer - Oldřich Baudyš";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label casLabel;
+        private System.Windows.Forms.Button startStopButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Timer stopkyTimer;
+    }
+}
+
